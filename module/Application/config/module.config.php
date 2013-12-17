640e28c9c3cde9447f5aa91b6c6f6095aeb3bf3e
Sasha <sasha@urbanmusic.de>
2013-12-17 13:17:37 +0100

    ACL Navigation

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -3,92 +3,119 @@
  * Zend Framework (http://framework.zend.com/)
  *
  * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
+
 return array(
-    'application' => array(
-        'name' => 'Training Center',
-        'version' => '1.0.0'
+    'application' => array (
+        'name' 	  => 'Training Center',
+        'version' => '0.0.1'
     ),
     'router' => array(
         'routes' => array(
             'home' => array(
                 'type' => 'Zend\Mvc\Router\Http\Literal',
                 'options' => array(
-                    'route' => '/',
+                    'route'    => '/',
                     'defaults' => array(
                         'controller' => 'Application\Controller\Index',
-                        'action' => 'index'
-                    )
-                )
+                        'action'     => 'index',
+                    ),
+                ),
             ),
             // The following is a route to simplify getting started creating
             // new controllers and actions without needing to create a new
             // module. Simply drop new controllers in, and you can access them
             // using the path /application/:controller/:action
             'application' => array(
-                'type' => 'Literal',
+                'type'    => 'Literal',
                 'options' => array(
-                    'route' => '/application',
+                    'route'    => '/application',
                     'defaults' => array(
                         '__NAMESPACE__' => 'Application\Controller',
-                        'controller' => 'Index',
-                        'action' => 'index'
-                    )
+                        'controller'    => 'Index',
+                        'action'        => 'index',
+                    ),
                 ),
                 'may_terminate' => true,
                 'child_routes' => array(
                     'default' => array(
-                        'type' => 'Segment',
+                        'type'    => 'Segment',
                         'options' => array(
-                            'route' => '/[:controller[/:action]]',
+                            'route'    => '/[:controller[/:action]]',
                             'constraints' => array(
                                 'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                                'action' => '[a-zA-Z][a-zA-Z0-9_-]*'
+                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
                             ),
-                            'defaults' => array()
-                        )
-                    )
-                )
-            )
-        )
+                            'defaults' => array(
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        ),
     ),
     'service_manager' => array(
         'factories' => array(
             'translator' => 'Zend\I18n\Translator\TranslatorServiceFactory',
             'cipher'     => 'Application\Service\Factory\SymmetricCipher',
-        )
+            'navigation' => 'Zend\Navigation\Service\DefaultNavigationFactory',
+            'text-cache' => 'Zend\Cache\Service\StorageCacheFactory',
+            'var-cache'	 => 'Application\Service\Factory\VariableCache',
+        ),
     ),
     'translator' => array(
         'locale' => 'en_US',
         'translation_file_patterns' => array(
             array(
-                'type' => 'gettext',
+                'type'     => 'gettext',
                 'base_dir' => __DIR__ . '/../language',
-                'pattern' => '%s.mo'
-            )
-        )
+                'pattern'  => '%s.mo',
+            ),
+        ),
     ),
     'controllers' => array(
         'invokables' => array(
             'Application\Controller\Index' => 'Application\Controller\IndexController'
-        )
+        ),
     ),
     'view_manager' => array(
         'display_not_found_reason' => true,
-        'display_exceptions' => true,
-        'doctype' => 'HTML5',
-        'not_found_template' => 'error/404',
-        'exception_template' => 'error/index',
+        'display_exceptions'       => true,
+        'doctype'                  => 'HTML5',
+        'not_found_template'       => 'error/404',
+        'exception_template'       => 'error/index',
         'template_map' => array(
-            'layout/layout' => __DIR__ . '/../view/layout/layout.phtml',
+            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
             'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
-            'error/404' => __DIR__ . '/../view/error/404.phtml',
-            'error/index' => __DIR__ . '/../view/error/index.phtml'
+            'error/404'               => __DIR__ . '/../view/error/404.phtml',
+            'error/index'             => __DIR__ . '/../view/error/index.phtml',
+            // paginator views
+            'paginator/sliding'       => __DIR__ . '/../view/paginator/sliding.phtml',
         ),
         'template_path_stack' => array(
-            __DIR__ . '/../view'
+            __DIR__ . '/../view',
+        ),
+    ),
+    'navigation' => array(
+        'default' => array(
+            array(
+                'label' => 'Home',
+                'route' => 'home',
+                'pages' => array (
+                    array(
+                        'label' => 'About',
+                        'route' => 'application/default',
+                        'controller' => 'index',
+                        'action' => 'about'
+                    ),
+                    array(
+                        'label' => 'Book',
+                        'uri'   => 'http://zf2consulting.com',
+                    )
+                )
+            ),
         )
-    )
+    ),
 );
